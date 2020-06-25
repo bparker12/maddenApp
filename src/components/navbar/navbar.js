7d5d97dcc43c6bf2517581f4807e6690b9cdc70f
Parkerbp <ben.p.parker12@gmail.com>
2020-06-25 17:22:40 -0500

    first init

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import { Menu } from 'semantic-ui-react'
+
+const navBar = props => {
+
+    return (
+        <nav>
+            <Menu>
+
+            </Menu>
+        </nav>
+    )
+}
