4524ea93b8f0f29af752c5db5d90e6a10286d791
Parkerbp <ben.p.parker12@gmail.com>
2020-06-29 18:19:56 -0500

    add elements to navbar with fillers for actual code

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,14 +1,32 @@
 import React from "react"
 import { Link } from "react-router-dom"
-import { Menu } from 'semantic-ui-react'
+import { Menu, Dropdown } from 'semantic-ui-react'
 
-const navBar = props => {
+const NavBar = props => {
 
     return (
         <nav>
             <Menu>
-
+                <Menu.Item header>MaddenApp</Menu.Item>
+                <Menu.Item 
+                    name='Profile'
+                />
+                <Dropdown item text='Franchise Select'>
+                    <Dropdown.Menu>
+                        <Dropdown.Item>Insert Franchises here</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                
+                <Menu.Menu position='right'>
+                    <Menu.Item header>
+                        Username here
+                    </Menu.Item>
+                    <Menu.Item 
+                        name='Logout'
+                    />
+                </Menu.Menu>
             </Menu>
         </nav>
     )
 }
+export default NavBar
--- src/maddenApp.js
+++ src/maddenApp.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import ApplicationViews from './applicationViews'
+import NavBar from './components/navbar/navbar'
 import './maddenApp.css'
 
 const MaddenApp = () => {
 
   return (
     <React.Fragment>
+      <Route render={props => (
+        <NavBar {...props} />
+      )}
+      />
       <Route render={props => (
         <ApplicationViews {...props} />
       )}
