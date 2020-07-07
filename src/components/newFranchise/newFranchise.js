2ff7999521b838a3fb24c9bee78bb3e376f64c0c
bparker12 <49100894+bparker12@users.noreply.github.com>
2020-07-07 09:59:33 -0500

    add modal and clean up inputs to go into state better for filling database with correct data

--- src/components/newFranchise/newFranchise.js
+++ src/components/newFranchise/newFranchise.js
@@ -1,35 +1,47 @@
 import React, { useEffect, useState, useRef } from "react";
 import post from '../../modules/apiManager'
-import { Form, Grid, Button, Label } from "semantic-ui-react";
+import { Form, Grid, Button, Label, Modal } from "semantic-ui-react";
 
 const NewFranchise = props => {
 
-    const name = useRef()
+    const name = useRef('')
 
-    const [onlineLeague, setOnlineLeague] = useState(true)
     const [maddenYear, setMaddenYear] = useState(20)
+    const [onlineLeague, setOnlineLeague] = useState(true)
     const [privateLeague, setPrivateLeague] = useState(true)
+    const [openModal, setModal] = useState(false)
 
     const [onlineOptions] = React.useState([
-        {label: 'True', value: true},
-        {label: 'False', value: false}
+        {label: 'Yes', value: true},
+        {label: 'No', value: false}
     ])
     const [maddenYearOption] = React.useState([
         {label: '20', value: 20},
         {label: '21', value: 21}
     ])
-    
+
     const changeValue = (e) => {
         if(e.target.id === 'online'){
-            setOnlineLeague(e.target.value)
+            if(e.target.value === 'true'){
+            setOnlineLeague(true)
+            } else {setOnlineLeague(false)
+            }
         } else if(e.target.id ==='year'){
-            setMaddenYear(e.target.value)
+            setMaddenYear(parseInt(e.target.value))
         }
         console.log(onlineLeague, maddenYear)
     }
-    
-    const toggleChecked = () => {
-        setPrivateLeague(!privateLeague)
+
+    const toggleChecked = (e) => {
+        if(e.target.id === 'checkBox'){
+            setPrivateLeague(!privateLeague)
+        } else if(e.target.id === 'submitModal'){
+            if(name.current.value === ''){
+                alert("Please fill in Blank")
+            }else{
+                setModal(!openModal)
+            }
+        }
         console.log(privateLeague)
     }
 
@@ -42,7 +54,6 @@ const NewFranchise = props => {
                         <Label size='big'>Madden #</Label>
                         <select
                             onChange={changeValue}
-                            value={maddenYear}
                             id="year"
                             >
                             {maddenYearOption.map(yearOpt => (
@@ -55,32 +66,64 @@ const NewFranchise = props => {
                             ))}
                         </select>
                     </Form.Field>
-                    <Label size ='large' prompt basic>Franchise Name</Label>
-                    <input required defaultValue="" placeholder='BensStrikeAgain' type='string' ref={name }
-
+                    <Form.Field>
+                    <Label size ='big'>Franchise Name</Label>
+                    <div>
+                    <input defaultValue="" placeholder='' type='string' id='franName' ref={name }
                     />
+                    </div>
+                    </Form.Field>
                     <Form.Field>
                         <Label size='big'>Online League</Label>
                         <select
                            onChange={changeValue}
                            value={onlineLeague}
                            id="online"
-                           > 
+                           >
                            {onlineOptions.map(opt => (
                                <option
                                key={opt.value}
                                value={opt.value}
-                               > 
-                               {opt.label} 
+                               >
+                               {opt.label}
                                </option>
                            ))}
                         </select>
                     </Form.Field>
                     <Form.Field>
                         <Label size='big'>Private League</Label>
-                        <Form.Checkbox defaultChecked defaultValue={privateLeague} slider onChange={toggleChecked} />
+                        <Form.Checkbox defaultChecked defaultValue={privateLeague} slider onChange={toggleChecked} id='checkBox' />
                     </Form.Field>
-                    <Button>Submit</Button>
+                    <Button
+                        id='submitModal'
+                        onClick={toggleChecked}
+                    >Submit
+                    </Button>
+                    <Modal
+                    open={openModal}
+                    onCancel={() => setModal(!openModal)}
+                    >
+                        <React.Fragment>
+                            <Modal.Content>
+                                <Modal.Description>
+                                    <h1> Name: {name.current.value} </h1>
+                                    <h3> Madden # {maddenYear} {"\n"} </h3>
+                                    <h3> Online League: {onlineLeague?'Yes':'No'} </h3>
+                                    <h3> Private: {privateLeague?'Yes':'No'}</h3>
+                                </Modal.Description>
+                                    <Button
+
+                                    >
+                                        Submit
+                                    </Button>
+                                    <Button
+                                        onClick={() => setModal(!openModal)}
+                                    >
+                                        Cancel
+                                    </Button>
+                            </Modal.Content>
+                        </React.Fragment>
+                    </Modal>
                 </Form>
             </Grid.Row>
         </Grid>
