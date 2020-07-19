ee4a8747ed11057e1dc208b6f17df4183c4a84e1
bparker12 <49100894+bparker12@users.noreply.github.com>
2020-07-19 18:46:41 -0500

    franchise is showing on the DOM and not pinging the DB constantly. Able to pull from DB the current franchise selected

--- src/components/franchiseHome/franchiseHome.js
+++ src/components/franchiseHome/franchiseHome.js
@@ -1,27 +1,47 @@
 import React, { useEffect, useState } from "react";
-// import { Form, Label, Grid, Button } from "semantic-ui-react";
+import { Dropdown, Grid, Button } from "semantic-ui-react";
 import apiManager from "../../modules/apiManager";
 
-const FranchiseHome = props => {
+const FranchiseHome = (props, param) => {
 
 
     const [currentFranchise, setCurrentFranchise] = useState([])
 
-    useEffect(() => {
-        getFranchise()}, [])
+    const franchiseUrl = props.match.params.franchiseName
 
     const getFranchise = () => {
-        const franchiseUrl = props.match.params.franchiseName
         apiManager.getAll("franchise", `name=${franchiseUrl}`)
         .then(setCurrentFranchise)
     }
 
+    useEffect(getFranchise, [franchiseUrl])
+
     return (
-        <div>
-        {currentFranchise.map((franchise) => (
-            <p>{franchise.name}</p>
-        ))}
-        </div>
+        <React.Fragment>
+        <Grid divided>
+            <Grid.Row columns={2}>
+                {currentFranchise.map((franchise) => (
+                    <Grid.Column
+                    key={franchise.id}
+                    >
+                    <h1 >{franchise.name}</h1>
+                    <p>Madden {franchise.year}</p>
+                    </Grid.Column>
+                ))}
+                <Grid.Column>
+                <h4>Dropdown here</h4>
+                    {/* <Dropdown>
+                        Years
+                    </Dropdown> */}
+                </Grid.Column>
+            </Grid.Row>
+            <Grid.Row columns={1}>
+                <Grid.Column  textAlign='center'>
+                    <h3>Current Years stories here in table?</h3>
+                </Grid.Column>
+            </Grid.Row>
+        </Grid>
+        </React.Fragment>
     )
 
 
